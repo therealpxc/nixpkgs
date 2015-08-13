92a9bc4a16f0b1c387b59baed14819cda7d4c959
Thomas Strobel <ts468@cam.ac.uk>
2015-08-13 04:03:29 +0200

    nixup: declarative user profile
    The work was started by Nicolas B. Pierron, see (github PR 4594).
    
    NixUP attempts to establish a declarative approach for defining user
    environments. It is for user environments what "nixos-rebuild" is for
    the NixOS system configuration.
    
    By default, the configuration is rooted in "$XDG_CONFIG_HOME/nixup/profile.nix".
    The command to manage the user environment is "nixup". By default,
    "nixup" operates on the "default" profile the operating user.
    
    A simple configuration for "$XDG_CONFIG_HOME/nixup/profile.nix" could be:
    
        { config, pkgs, ... }:
    
        {
    
          user.packages = with pkgs; [ vim mpd ];
    
          imperativeNix.enable = true;
    
          user.resourceFiles = [
            { target = ".mpdconf";
              text = ''
                music_directory         "~/.mpd/music"
                playlist_directory      "~/.mpd/playlists"
                db_file                 "~/.mpd/mpd.db"
                pid_file                "~/.mpd/mpd.pid"
                state_file              "~/.mpd/state"
                bind_to_address         "localhost"
                gapless_mp3_playback    "yes"
                input {
                    plugin   "curl"
                }
                audio_output {
                    type     "pulse"
                    name     "My Pulse Output"
                }
                mixer_type              "hardware"
                volume_normalization    "yes"
                filesystem_charset      "UTF-8"
              '';
            }
          ];
    
          systemd.services.mpd = {
            wantedBy = [ "default.target" ];
            description = "Music Player Daemon";
            path = [ pkgs.mpd ];
            preStart = "mkdir -p ~/.mpd";
            script = "exec mpd --no-daemon";
          };
    
        }

--- nixos/modules/services/x11/display-managers/gdm.nix
+++ nixos/modules/services/x11/display-managers/gdm.nix
@@ -154,7 +154,7 @@ in
         password required       pam_deny.so
 
         session  required       pam_succeed_if.so audit quiet_success user = gdm
-        session  required       pam_env.so envfile=${config.system.build.pamEnvironment}
+        session  required       pam_env.so readenv=0 conffile=${config.system.build.pamEnvironment}
         session  optional       ${pkgs.systemd}/lib/security/pam_systemd.so
         session  optional       pam_keyinit.so force revoke
         session  optional       pam_permit.so
@@ -162,7 +162,7 @@ in
 
       gdm.text = ''
         auth     requisite      pam_nologin.so
-        auth     required       pam_env.so envfile=${config.system.build.pamEnvironment}
+        auth     required       pam_env.so envfile=${config.system.build.pamEnvironment} readenv=0
 
         auth     required       pam_succeed_if.so uid >= 1000 quiet
         auth     optional       ${gnome3.gnome_keyring}/lib/security/pam_gnome_keyring.so
@@ -179,7 +179,7 @@ in
         ${optionalString config.security.pam.enableEcryptfs
           "password optional ${pkgs.ecryptfs}/lib/security/pam_ecryptfs.so"}
 
-        session  required       pam_env.so envfile=${config.system.build.pamEnvironment}
+        session  required       pam_env.so readenv=0 conffile=${config.system.build.pamEnvironment}
         session  required       pam_unix.so
         ${optionalString config.security.pam.enableEcryptfs
           "session optional ${pkgs.ecryptfs}/lib/security/pam_ecryptfs.so"}
@@ -190,7 +190,7 @@ in
 
       gdm-password.text = ''
         auth     requisite      pam_nologin.so
-        auth     required       pam_env.so envfile=${config.system.build.pamEnvironment}
+        auth     required       pam_env.so readenv=0 conffile=${config.system.build.pamEnvironment}
 
         auth     required       pam_succeed_if.so uid >= 1000 quiet
         auth     optional       ${gnome3.gnome_keyring}/lib/security/pam_gnome_keyring.so
@@ -206,7 +206,7 @@ in
         ${optionalString config.security.pam.enableEcryptfs
           "password optional ${pkgs.ecryptfs}/lib/security/pam_ecryptfs.so"}
 
-        session  required       pam_env.so envfile=${config.system.build.pamEnvironment}
+        session  required       pam_env.so readenv=0 conffile=${config.system.build.pamEnvironment}
         session  required       pam_unix.so
         ${optionalString config.security.pam.enableEcryptfs
           "session optional ${pkgs.ecryptfs}/lib/security/pam_ecryptfs.so"}
@@ -226,7 +226,7 @@ in
         password requisite      pam_unix.so nullok sha512
 
         session  optional       pam_keyinit.so revoke
-        session  required       pam_env.so envfile=${config.system.build.pamEnvironment}
+        session  required       pam_env.so readenv=0 conffile=${config.system.build.pamEnvironment}
         session  required       pam_unix.so
         session  required       pam_loginuid.so
         session  optional       ${pkgs.systemd}/lib/security/pam_systemd.so

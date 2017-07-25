51ef96a98fbb7379882c2d4c075b795aab77ad05
Patrick Callahan <patrick.callahan@sigfig.com>
2017-07-24 21:55:10 -0700

    chips: init at 1.1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5141,6 +5141,8 @@ with pkgs;
     bootstrap-chicken = chicken.override { bootstrap-chicken = null; };
   };
 
+  chips = haskellPackages.callPackage ../shells/chips/default.nix { };
+
   egg2nix = callPackage ../development/tools/egg2nix {
     chickenEggs = callPackage ../development/tools/egg2nix/chicken-eggs.nix { };
   };
